3d4b57d97f7d90d6016207f192ea18a567695e85
Nick Hibberd <nicholas.hibberd@ventyx.abb.com>
2013-12-24 11:36:32 +1000

    Adding mzero

--- src/Dashboardh/Prelude.hs
+++ src/Dashboardh/Prelude.hs
@@ -5,3 +5,4 @@ module Dashboardh.Prelude (
 import Prelude as X (Int, Char, String, Eq, Ord, Show (..), IO, ($), id, putStrLn, (.), error)
 import Control.Applicative as X ((<$>), (<*>), (*>), (<*), pure)
 import Control.Monad.Trans as X (liftIO)
+import Control.Monad as X (mzero)
