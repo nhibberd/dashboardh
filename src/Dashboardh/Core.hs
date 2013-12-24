3a4d884da77bb5eb665fda2b5223ff39f89e610b
Nick Hibberd <nicholas.hibberd@ventyx.abb.com>
2013-12-24 11:11:35 +1000

    Adding dashboardh Core

---
+++ src/Dashboardh/Core.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Dashboardh.Core (
+  site
+) where
+
+import Data.Text.Lazy       (Text) 
+import Web.Scotty           hiding (body)
+
+
+site :: Text -> ActionM()
+site a = 
+    html a
