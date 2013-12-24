e5490fd8f9d3d0fa01360183fbbd7c5085ef620f
Nick Hibberd <nicholas.hibberd@ventyx.abb.com>
2013-12-25 09:05:15 +1000

    Adding stubs for api calls

--- src/Dashboardh/Status.hs
+++ src/Dashboardh/Status.hs
@@ -23,3 +23,28 @@ instance FromJSON Job where
     parseJSON _          = mzero
 instance ToJSON Job where
     toJSON (Job n bl ba bmin bmax) = object ["name" .= n, "buildTimeLatest" .= bl, "buildTimeAvg" .= ba, "buildTimeMin" .= bmin, "buildTimeMax" .= bmax]
+
+
+getSimpleJob :: Text -> Job
+getSimpleJob =
+    error("handle jenkins json")
+
+
+getJobStatus :: Text -> Int
+getJobStatus =
+    error("return current build status")
+
+-- Wrap following operations in Json call to jenkins
+
+doStuff :: Text -> (Text -> Int) -> Int
+doStuff =
+    error("wrapper . handle jenkins api")
+
+getJobStatusHistory :: Text -> Int
+getJobStatusHistory =
+    error("return list of build status")    
+
+getJobTimeHistory :: Text -> Int
+getJobTimeHistory =
+    error("return list of build times")    
+
