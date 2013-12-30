77bd9b339677eb4296275eefb63345914e39ec2d
Nick Hibberd <nhibberd@gmail.com>
2013-12-30 11:27:09 +1000

    Extending prelude

--- src/Dashboardh/Prelude.hs
+++ src/Dashboardh/Prelude.hs
@@ -2,7 +2,27 @@ module Dashboardh.Prelude (
   module X
 ) where
 
-import Prelude as X (Int, Char, String, Eq, Ord, Show (..), IO, ($), id, putStrLn, (.), error)
+import Prelude as X (
+      Int
+    , Char
+    , String
+    , Eq
+    , Ord
+    , Show (..)
+    , IO
+    , ($)
+    , id
+    , putStrLn
+    , (.)
+    , error
+    , Either( Right, Left )
+    , undefined
+    , return
+    , map
+    , (>>=)
+    , (=<<)
+    , foldr
+    )
 import Control.Applicative as X ((<$>), (<*>), (*>), (<*), pure)
 import Control.Monad.Trans as X (liftIO)
 import Control.Monad as X (mzero)
